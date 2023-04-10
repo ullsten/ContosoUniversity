10c3bbb0f122791b77e180f36d4a3df2369e4b67
ullzten <ullzten@gmail.com>
2023-04-10 21:55:04 +0200

    Add project files.

---
+++ ContosoUniversity/Views/Students/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Views.Students
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ContosoUniversity.SchoolContext _context;
+
+        public DetailsModel(ContosoUniversity.SchoolContext context)
+        {
+            _context = context;
+        }
+
+      public Student Student { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Student == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students.FirstOrDefaultAsync(m => m.ID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Student = student;
+            }
+            return Page();
+        }
+    }
+}
